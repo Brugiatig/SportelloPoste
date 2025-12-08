93bfd769955ce56f7968b3a3044c1ab33c3fc82e
Brugiatig <giuliobrugiati5@gmail.com>
2025-12-08 18:34:54 +0100

    Update ListaClienti.java

--- src/ListaClienti.java
+++ src/ListaClienti.java
@@ -1,46 +1,72 @@
-// ListaClienti.java
 import java.util.ArrayList;
-
 /**
- * Classe che rappresenta la risorsa condivisa fra i thread
+ * Classe che rappresenta la risorsa condivisa fra i due thread
+ * da gestire con metodi "synchronized"
+ * e con l'uso di wait() e notify()
  * @author frida
+ * @version 1.0
  */
 public class ListaClienti {
+    /*variabili d'istanza*/
     private ArrayList<Integer> listaNumeri;
     private int ultimoArrivo;
-    private final int numeroMassimo = 10;
-
+    private int ultimoServito;
+    private final int numeroMassimo = 6;
+    /**
+     * constructor
+     * settaggio delle variabili di istanza
+     */
     public ListaClienti() {
-        listaNumeri = new ArrayList<>();
+        listaNumeri = new ArrayList<Integer>();
         ultimoArrivo = 0;
+        ultimoServito = 0;
     }
 
+    /*synchronized parola chiave che gestisce il meccanismo del lock, ovvero
+    * 1) impedisce ad un altro thread l'esecuzione di tale
+    * metodo, se un precedente thread lo sta già eseguendo
+    * 2) senza di lui wait e notify non possono essere usati
+    * si genera l'eccezione : IllegalMonitorStateException,*/
+
     /**
-     * Metodo eseguito dai thread Sportello
-     * Rimuove il primo cliente dalla lista (FIFO)
+     * metodo eseguito da un thread della Classe Sportello
+     * si chiede se è in coda un altro cliente dopo l'ultimo che ha servito
+     * se c'è lo server (incrementa di uno ultimoServito)
+     * else resta di attesa che arrivi...(notify)
+     * @return Integer ultimoServito synchronized
      */
-    public synchronized Integer rimuoviCliente() throws InterruptedException {
-        while (listaNumeri.isEmpty()) { // uso lista reale invece dei contatori
-           // System.out.println("Non ci sono clienti in attesa");
+    public synchronized Integer rimuoviCliente(String nome) throws InterruptedException{
+        while (ultimoServito >= ultimoArrivo) {
+            System.out.println("non ci sono arrivi dopo l'ultimo servito");
             wait();
         }
-        Integer cliente = listaNumeri.remove(0); // rimuove il primo cliente FIFO
-        return cliente;
+        ultimoServito++;
+        System.out.println("Servito Cliente Numero \t " + ultimoServito+
+                " dallo sportello "+ nome);
+        return ultimoServito;
+    /*while (listaNumeri.isEmpty()){
+        System.out.println("non ci sono arrivi dopo l'ultimo servito");
+        wait();
+    }
+        ultimoServito++;
+        return ultimoServito;*/
     }
 
+
+
     /**
-     * Metodo eseguito da GestoreArrivi
-     * Aggiunge un cliente alla lista
+     * metodo eseguito da un thread della Classe GestoreArrivi
+     * che produce un nuovo int aggiungendo 1 all'ultimoArrivo
+     * e inserisce tale nuovo numero / ticket nella lista numeri
+     * @return Integer: ultimoArrivo o null se gli arrivi saturano
      */
-    public synchronized Integer addCliente() {
+    public synchronized Integer addCliente(){
         if (ultimoArrivo < numeroMassimo) {
             ultimoArrivo++;
             listaNumeri.add(ultimoArrivo);
-            System.out.println("Arrivo Cliente Numero \t " + ultimoArrivo + " (gestore: " + Thread.currentThread().getName() + ")");
-            notifyAll();
+            notify();
             return ultimoArrivo;
         }
         return null;
     }
-
 }
